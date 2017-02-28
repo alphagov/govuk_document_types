0f153f1b1a2ad4e20e60de5d471890ea9a7ee8ee
Tijmen Brommet <tijmen@gmail.com>
2017-02-28 15:11:56 +0000

    Add document type groupings

--- spec/govuk_document_types_spec.rb
+++ spec/govuk_document_types_spec.rb
@@ -4,4 +4,18 @@ describe GovukDocumentTypes do
   it "has a version number" do
     expect(GovukDocumentTypes::VERSION).not_to be nil
   end
+
+  describe '.supertypes' do
+    it 'returns a supertype for a known document type' do
+      supertypes = GovukDocumentTypes.supertypes(document_type: 'detailed_guide')
+
+      expect(supertypes).to eql({ "navigation_document_supertype" => "guidance" })
+    end
+
+    it 'returns nil for a known document type' do
+      supertypes = GovukDocumentTypes.supertypes(document_type: 'something_not_there')
+
+      expect(supertypes).to eql({ "navigation_document_supertype" => nil })
+    end
+  end
 end
