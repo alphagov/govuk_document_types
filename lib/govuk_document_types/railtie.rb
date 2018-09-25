c74dda5ac3d0ef213fc4dfa97d93a6d9bcb63164
Andrew Garner <andrew@andrewgarner.com>
2018-09-25 10:15:16 +0100

    WIP

---
+++ lib/govuk_document_types/railtie.rb
@@ -0,0 +1,25 @@
+require 'rails'
+
+module GovukDocumentTypes
+  class Railtie < ::Rails::Railtie #:nodoc:
+    initializer 'govuk_document_types' do |app|
+      GovukDocumentTypes::Railtie.instance_eval do
+        pattern = pattern_from app.config.i18n.available_locales
+
+        add("rails/locale/#{pattern}.yml")
+      end
+    end
+
+  protected
+
+    def self.add(pattern)
+      files = Dir[File.join(File.dirname(__FILE__), '../..', pattern)]
+      I18n.load_path.concat(files)
+    end
+
+    def self.pattern_from(args)
+      array = Array(args || [])
+      array.blank? ? '*' : "{#{array.join ','}}"
+    end
+  end
+end
